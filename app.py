b2fa2faffabdcc134980403b958a781f87f4f95f
Anastasia <anlemett@gmail.com>
2022-11-24 16:29:14 +0100

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello this is the new version!"
+    return "Hello!"
 
 #if __name__ == '__main__':
 #    app.run(debug=DEBUG, host=HOST, port=PORT)
