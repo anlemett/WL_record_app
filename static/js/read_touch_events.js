c92e2609a3a8d811f0fb1fdc3afc2047be6508a8
anlemett <anlemett@gmail.com>
2022-12-09 15:55:57 +0100

    10 buttons

--- static/js/read_touch_events.js
+++ static/js/read_touch_events.js
@@ -1,6 +1,14 @@
 const elementOne = document.getElementById("one");
 const elementTwo = document.getElementById("two");
 const elementThree = document.getElementById("three");
+const elementFour = document.getElementById("four");
+const elementFive = document.getElementById("five");
+const elementSix = document.getElementById("six");
+const elementSeven = document.getElementById("seven");
+const elementEight = document.getElementById("eight");
+const elementNine = document.getElementById("nine");
+const elementTen = document.getElementById("ten");
+
 const output = document.getElementById("output");
 const clearer = document.getElementById("clear");
 
@@ -38,3 +46,10 @@ watchEvent(elementTwo, "click");
 watchEvent(elementThree, "click");
 //watchEvent(elementThree, "focus");
 //watchEvent(elementThree, "blur");
+watchEvent(elementFour, "click");
+watchEvent(elementFive, "click");
+watchEvent(elementSix, "click");
+watchEvent(elementSeven, "click");
+watchEvent(elementEight, "click");
+watchEvent(elementNine, "click");
+watchEvent(elementTen, "click");
